9125ab860f7946814c54cf61f4083416417d735e
DEPGames-official <dreyespoppergames@gmail.com>
2023-07-26 15:10:27 +0200

    Created Undirected graph with a Breadth First Search (BFS)

---
+++ Undirected Graph/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Undirected_Graph
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            UndirectedGraph<string> graph = new UndirectedGraph<string>();
+
+            graph.AddNew("Michael");
+            graph.AddNew("Charles");
+            graph.AddNew("Peter");
+            graph.AddNew("Mushroom");
+            graph.AddNew("Potato");
+            graph.AddNew("Candice");
+            graph.AddNew("Dylan");
+            graph.AddNew("Xavier");
+            graph.AddNew("Sean");
+            graph.AddNew("Louis");
+            graph.AddNew("Mieka");
+            graph.AddNew("Georgia");
+
+
+            graph.AddExistRelationship("Charles", "Michael");
+
+            graph.AddExistRelationship("Peter", "Potato");
+
+            graph.AddExistRelationship("Mushroom", "Candice");
+
+            graph.AddExistRelationship("Mieka", "Sean");
+
+            graph.AddExistRelationship("Georgia", "Xavier");
+
+            graph.AddExistRelationship("Louis", "Dylan");
+
+            //graph.PrintNodeValues();
+        }
+    }
+}
---
+++ Undirected Graph/UndirectedGraph.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Undirected_Graph
+{
+    public class UndirectedGraph<T>
+    {
+        //List to hold all nodes
+        List<Node<T>> nodes = new List<Node<T>>();
+
+        public void AddNew(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+            //Add node to nodes list
+            nodes.Add(newNode);
+        }
+
+        //Used to add a relationship with 2 existing nodes
+        public void AddExistRelationship(T currentData, T relationshipData)
+        {
+
+            //Check to make sure they aren't equal
+            if (!currentData.Equals(relationshipData))
+            {
+
+                //Search for nodes using BFS to check if they exist
+                Node<T>? currentDataNode = SearchGraph(currentData);
+                Node<T>? relationshipDataNode = SearchGraph(relationshipData);
+
+                //Another check to make sure its not null
+                if (currentDataNode != null && relationshipDataNode != null)
+                {
+                    System.Console.WriteLine("Current data node found: " + currentDataNode.data);
+                    System.Console.WriteLine("Current relationship data node found: " + relationshipDataNode.data);
+
+                    currentDataNode.AddRelationship(relationshipDataNode);
+                    relationshipDataNode.AddRelationship(currentDataNode);
+                }
+            }
+        }
+
+        //Search graph using Breadth First Search (BFS)
+        Node<T>? SearchGraph(T data)
+        {
+            //Hashset that holds all visited nodes
+            HashSet<Node<T>> visited = new HashSet<Node<T>>();
+
+            foreach (Node<T> node in nodes)
+            {
+                if (visited.Contains(node))
+                {
+                    continue;
+                }
+                else if (!visited.Contains(node) && !node.data.Equals(data))
+                {
+                    //If node isnt visited start BFS at node to avoid stray nodes
+                    Node<T>? valueSearch = BFSFromNode(node, visited, data);
+
+                    if (valueSearch != null)
+                        return valueSearch;
+
+                    continue;
+                }
+
+                return node;
+            }
+            return null;
+        }
+
+        //Do full BFS from particular node
+        Node<T>? BFSFromNode(Node<T> startNode, HashSet<Node<T>> visited, T dataToFind)
+        {
+            //Create queue as need for BFS
+            Queue<Node<T>> queue = new Queue<Node<T>>();
+
+            queue.Enqueue(startNode);
+
+            while (queue.Count > 0)
+            {
+                Node<T> current = queue.Dequeue();
+                visited.Add(current);
+
+                System.Console.WriteLine(current.data);
+
+                if (current.data.Equals(dataToFind))
+                {
+                    return current;
+                }
+
+                foreach (Node<T> relationship in current.nodeRelationships)
+                {
+
+                    if (!visited.Contains(relationship))
+                    {
+                        queue.Enqueue(relationship);
+                        continue;
+                    }
+                }
+
+            }
+            return null;
+        }
+
+        public void PrintNodeValues()
+        {
+            System.Console.WriteLine();
+            foreach (Node<T> node in nodes)
+            {
+                System.Console.WriteLine(node.data);
+            }
+        }
+    }
+
+    class Node<T>
+    {
+        public List<Node<T>> nodeRelationships;
+        public T data { get; }
+        public Node(T? data)
+        {
+            this.data = data;
+            this.nodeRelationships = new List<Node<T>>();
+        }
+
+        public void AddRelationship(Node<T> relationshipNode)
+        {
+            //Add relationshipNode to nodeRelationships list
+            nodeRelationships.Add(relationshipNode);
+        }
+    }
+}
