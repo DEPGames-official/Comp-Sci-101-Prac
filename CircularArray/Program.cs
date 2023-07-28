81c733ed964092eb5744d98c42ec16ab1c93e182
DEPGames-official <dreyespoppergames@gmail.com>
2023-07-28 12:59:26 +0200

    Fixed namespace naming

---
+++ CircularArray/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CircularArray
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CircularArray<int> array = new CircularArray<int>(5);
+            array.Add(5);
+            array.Add(2);
+            array.Add(3);
+            array.Add(4);
+            array.Add(6);
+
+            array.PrintArr();
+
+            array.Remove();
+            array.Remove();
+            array.Remove();
+            array.Remove();
+
+            array.PrintArr();
+        }
+    }
+}
