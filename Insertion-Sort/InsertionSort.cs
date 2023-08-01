14b67778d6d5e18ed7d9f05e73b3dc88a5c7a6a8
DEPGames-official <dreyespoppergames@gmail.com>
2023-08-01 09:41:51 +0200

    Added comments where appropiate

--- Insertion-Sort/InsertionSort.cs
+++ Insertion-Sort/InsertionSort.cs
@@ -5,17 +5,19 @@ using System.Threading.Tasks;
 
 namespace Insertion_Sort
 {
+    /// <summary>
+    /// Represents a Insertion Sort Algorithm
+    /// </summary>
     public class InsertionSort
     {
         public int[] Sort(int[] arr)
         {
-            int num;
+            //Goes through every value in array
             for (int i = 1; i < arr.Length; i++)
             {
-                num = i;
+                //Checks if smaller than values on left and swaps
                 for (int x = i; x > 0; x--)
                 {
-
                     if (arr[x] < arr[x - 1])
                     {
                         (arr[x], arr[x - 1]) = (arr[x - 1], arr[x]);
